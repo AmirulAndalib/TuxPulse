7d687a179028672f7f9f6cb2cb98619c6cbd3ae8
eoliann <eoliann@yahoo.com>
2026-03-25 12:18:23 +0200

    Delete build_flatpak.sh

--- build_flatpak.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-APPID=ro.eoliann.TuxPulse
-flatpak-builder --force-clean build-dir flatpak/$APPID.json
-flatpak build-export repo build-dir
-flatpak build-bundle repo tuxpulse.flatpak $APPID
-echo "Flatpak build complete: tuxpulse.flatpak"
